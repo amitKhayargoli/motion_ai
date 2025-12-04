f0592a2c2f01033bbed7027b4589949fa9b4da40
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-12-04 22:21:14 +0545

    completed splash screen with animation

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:motion_ai/screens/onboarding_screen.dart';
+import 'package:motion_ai/screens/splash_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnboardingScreen(),
+      home: SplashScreen(),
     );
   }
 }
