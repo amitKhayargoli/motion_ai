67dc6ded69a3da05289b96346a9dae2c0f7dbbae
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-12-04 23:48:56 +0545

    fixed import

--- lib/feature/splash/view/splash_screen.dart
+++ lib/feature/splash/view/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:motion_ai/feature/onboarding/view/onboarding_screen.dart';
+import '../../../screens/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
