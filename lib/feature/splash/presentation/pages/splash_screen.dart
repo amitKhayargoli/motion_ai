b1154fcf9bd05d473e6365fc48c2de7d16edaac9
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-12-24 13:05:11 +0545

    Add splash screen implementation and related directories

--- lib/feature/splash/presentation/pages/splash_screen.dart
+++ lib/feature/splash/presentation/pages/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../onboarding/onboarding_screen.dart';
+import '../../../onboarding/presentation/pages/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
