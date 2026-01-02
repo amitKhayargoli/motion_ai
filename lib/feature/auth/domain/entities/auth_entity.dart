a012dd693759a54f14be3bb0296ebee98c5f7bad
Amit Khayargoli <khayargoliamit99@gmail.com>
2026-01-02 22:03:11 +0545

    Changes in Auth Entity and Repository in the Domain Layer

--- lib/feature/auth/domain/entities/auth_entity.dart
+++ lib/feature/auth/domain/entities/auth_entity.dart
@@ -1,22 +1,18 @@
 import 'package:equatable/equatable.dart';
 
-enum userRole { admin, user }
-
-class UserEntity extends Equatable {
+class AuthEntity extends Equatable {
   final String? userId;
   final String email;
   final String password;
-  final userRole? role;
   final DateTime? createdAt;
 
-  const UserEntity({
+  const AuthEntity({
     this.userId,
     required this.email,
     required this.password,
-    this.role,
     this.createdAt,
   });
 
   @override
-  List<Object?> get props => [userId, email, password, role, createdAt];
+  List<Object?> get props => [userId, email, password, createdAt];
 }
