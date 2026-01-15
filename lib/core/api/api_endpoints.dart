03ed98d0d850979b2af9b6e5096654c0fc2fe295
Amit Khayargoli <khayargoliamit99@gmail.com>
2026-01-15 19:44:48 +0545

    Added API Auth endpoints

---
+++ lib/core/api/api_endpoints.dart
@@ -0,0 +1,18 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  // Base URL - change this for production
+  static const String baseUrl = 'http://10.0.2.2:3000/api';
+  //static const String baseUrl = 'http://localhost:3000/api/v1';
+  // For Android Emulator use: 'http://10.0.2.2:3000/api/v1'
+  // For iOS Simulator use: 'http://localhost:5000/api/v1'
+  // For Physical Device use your computer's IP: 'http://192.168.x.x:5000/api/v1'
+
+  static const Duration connectionTimeout = Duration(seconds: 30);
+  static const Duration receiveTimeout = Duration(seconds: 30);
+
+  // ============ Student Endpoints ============
+  static const String login = '/auth/login';
+  static const String register = '/auth/register';
+  static const String getAllUsers = '/auth/users';
+}
