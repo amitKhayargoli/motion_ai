a602453775cb48596e84d42fc19d5381e16257aa
Amit Khayargoli <khayargoliamit99@gmail.com>
2026-02-22 15:58:38 +0545

    Add WorkspaceEntity class to define workspace data structure

---
+++ lib/feature/workspace/domain/entities/workspace_entity.dart
@@ -0,0 +1,11 @@
+class WorkspaceEntity {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  WorkspaceEntity({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+}
