c8c5bc0938bf8b7d3e2321df1ec36b201a81ce05
Amit Khayargoli <khayargoliamit99@gmail.com>
2026-03-01 17:59:11 +0545

    Added sensor providers

---
+++ lib/feature/home/presentation/providers/wave_to_switch_provider.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/legacy.dart';
+
+/// Whether the "wave to switch workspace" feature is enabled.
+final waveToSwitchEnabledProvider = StateProvider<bool>((_) => true);
---
+++ lib/feature/notes/presentation/providers/shake_to_refresh_provider.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/legacy.dart';
+
+/// Whether the "shake to refresh notes" feature is enabled.
+final shakeToRefreshEnabledProvider = StateProvider<bool>((_) => true);
